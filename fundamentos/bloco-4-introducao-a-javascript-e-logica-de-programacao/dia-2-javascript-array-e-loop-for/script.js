706d068fd7eab05ce8de488587422f80eae13f99
hector <hectoreddbenitez@gmail.com>
2021-06-25 18:43:08 -0300

    Exercsicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -53,3 +53,9 @@ if (let somatoria > 20){
 } else {
         console.log('Valor menor ou igual a 20');
 } 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+numeroMaoir = 0
+
+for(let index = 0)
