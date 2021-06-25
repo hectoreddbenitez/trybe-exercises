bf15dec1c46774d61f6a86e5e01ef24df9741e47
hector <hectoreddbenitez@gmail.com>
2021-06-25 16:04:52 -0300

    .

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,55 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 0; index < numbers.length; index += 1){
+
+
+console.log(numbers[index]);
+
+}
+
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somatoria = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+
+    somatoria += numbers[index]
+}
+
+console.log(somatoria)
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somatoria = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+
+    somatoria += numbers[index]
+}
+
+let result = somatoria / numbers.length
+
+console.log(result)
+
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somatoria = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+
+    somatoria += numbers[index]
+}
+
+let result = somatoria / numbers.length;
+
+
+if (let somatoria > 20){
+    console.log('Valor maior que 20');
+} else {
+        console.log('Valor menor ou igual a 20');
+} 
